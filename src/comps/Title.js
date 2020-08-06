12f82e1185b7f1b96b8ace035e20e211bfcedf7e
Ninad <ninadusatam@gmail.com>
2020-08-06 22:46:19 +0530

    added react bootstrap

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
 
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>
+        <Navbar bg="danger" variant="dark">
+          <Navbar.Brand href="/">
+            FireGram
+          </Navbar.Brand>
+        </Navbar>
+      </h1>
       <h2>Your Pictures</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
   import * as firebase from 'firebase/app';
   import 'firebase/storage';
   import 'firebase/firestore';
+  import 'firebase/auth';
 
   
   // Your web app's Firebase configuration
@@ -16,8 +17,10 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  const projectAuth = firebase.auth();
   const projectStorage = firebase.storage();
   const projectFirestore = firebase.firestore();
+  
   const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-  export { projectStorage, projectFirestore, timestamp };
+  export { projectStorage, projectFirestore, projectAuth, timestamp };
